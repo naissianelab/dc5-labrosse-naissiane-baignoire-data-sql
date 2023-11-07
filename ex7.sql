9dbfaaaed372adc3d1f5ee80372a0c824f018c71
naissianelab <144322684+naissianelab@users.noreply.github.com>
2023-11-07 16:23:29 +0100

    Update ex7.sql

--- ex7.sql
+++ ex7.sql
@@ -1,5 +1,6 @@
 -- Lister les entités et leurs pays d'origine :
 SELECT entity.name, address.countries
 FROM entity
-INNER JOIN address ON entity.id_address = address.id_address
+INNER JOIN address 
+  ON entity.id_address = address.id_address
 WHERE address.address IS NOT NULL AND address.address != ''
