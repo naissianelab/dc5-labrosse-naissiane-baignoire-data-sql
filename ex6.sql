57d7028dd1508078cbc576fbdd30f80470849c4c
naissianelab <naisianelabrosse@gmail.com>
2023-11-07 15:10:44 +0100

    Intermédiaires avec adresses associées

---
+++ ex6.sql
@@ -0,0 +1,5 @@
+-- Trouver tous les intermédiaires et leurs adresses associées :
+SELECT intermediary.name, address.address
+FROM intermediary
+INNER JOIN address ON intermediary.id_address = address.id_address
+WHERE address.address IS NOT NULL AND address.address != ''
