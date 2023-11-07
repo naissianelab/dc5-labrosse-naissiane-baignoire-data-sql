9a8ec8bf3594c2b1c58fde9112299e50f139ed22
naissianelab <naisianelabrosse@gmail.com>
2023-11-07 15:26:37 +0100

    Liste entités avec pays d'origine

---
+++ ex7.sql
@@ -0,0 +1,5 @@
+-- Lister les entités et leurs pays d'origine :
+SELECT entity.name, address.countries
+FROM entity
+INNER JOIN address ON entity.id_address = address.id_address
+WHERE address.address IS NOT NULL AND address.address != ''
