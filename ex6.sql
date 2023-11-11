4ed9ebb804a0947b44ebf92c39c6f84f55fac102
naissianelab <144322684+naissianelab@users.noreply.github.com>
2023-11-11 17:21:21 +0100

    intermédiaires et leurs adresses associées

--- ex6.sql
+++ ex6.sql
@@ -3,4 +3,5 @@ SELECT intermediary.name, address.address
 FROM intermediary
 INNER JOIN address 
   ON intermediary.id_address = address.id_address
-WHERE address.address IS NOT NULL AND address.address != ''
+WHERE address.address IS NOT NULL 
+  AND address.address != ''
